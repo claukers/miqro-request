3346d3b5ff3e5a1fb94a4903c9b7a0b666242f17
claukers <claukers@github>
2022-02-27 04:00:17 -0300

    cache requestImpl and avoid isBrowser check

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,25 @@ import {isBrowser} from "./helpers";
 
 export {RequestOptions, RequestResponse} from "./common";
 
+let requestImpl: undefined | any;
+
 export function request(options: RequestOptions, logger?: {
   error: (...args: any) => void;
   debug: (...args: any) => void;
 } | Console): Promise<RequestResponse> {
-  if (isBrowser()) {
-    const {request} = require("./fetch");
-    return request(options, logger);
+  if (requestImpl) {
+    return requestImpl(options, logger);
   } else {
-    // we use dynamic require to trick webpack into not bundle ./node.js
-    const nodePath = "./node";
-    const {request} = require(nodePath);
-    return request(options, logger);
+    if (isBrowser()) {
+      const {request} = require("./fetch");
+      requestImpl = request;
+      return request(options, logger);
+    } else {
+      // we use dynamic require to trick webpack into not bundle ./node.js
+      const nodePath = "./node";
+      const {request} = require(nodePath);
+      requestImpl = request;
+      return request(options, logger);
+    }
   }
 }
